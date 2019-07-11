33d34bb32fa78ca03a94f5116ca76ec20ab9b9a0
DANIELA <danielamiritescu@gmail.com>
2019-07-11 14:54:02 +0100

    save movie method complete

--- start_code/console.rb
+++ start_code/console.rb
@@ -7,12 +7,14 @@ require ( 'pry-byebug' )
 star1 = Star.new({ 'first_name' => 'Tom', 'last_name' => 'Hanks'})
 star1.save()
 
-
 star2 = Star.new({ 'first_name' => 'Robert', 'last_name' => 'De Niro'})
 star2.save()
 
+movie1 = Movie.new({'title' => 'Castaway', 'genre' => 'Adventure'})
+movie1.save()
 
-
+movie2 = Movie.new({'title' => 'Godfather', 'genre' => 'Thriller'})
+movie2.save()
 
 
 
--- start_code/models/movie.rb
+++ start_code/models/movie.rb
@@ -7,13 +7,21 @@ class Movie
 
 
 
-  def initialize
+  def initialize(options)
     @id = options['id'].to_i if options ['id']
     @title = options['title']
     @genre = options['genre']
 
   end
 
+  def save()
+    sql = "INSERT INTO movies(title, genre)
+          VALUES ($1, $2)
+          RETURNING id"
+    values = [@title, @genre]
+    movie = SqlRunner.run(sql, values).first
+    @id = movie['id'].to_i
+  end
 
 
 end
